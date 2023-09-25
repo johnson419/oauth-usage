79178e4067f6d9e2c913c1e703284357136e4b8a
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-09-25 13:41:19 +0300

    other configs

--- app/pages/api/auth/[...nextauth].js
+++ app/pages/api/auth/[...nextauth].js
@@ -9,5 +9,11 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async session(session, user) {
+      session.user.id = user.id
+      return session
+    },
+  },
 }
 export default NextAuth(authOptions)
